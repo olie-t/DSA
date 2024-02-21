34118158ed4f8e816d0bcb3b1f4ed1bd50c68caf
Olie <olthomas13@gmail.com>
2024-02-21 18:37:15 +0000

    working on recursion

--- Recursion.py
+++ Recursion.py
@@ -1,10 +1,27 @@
 
-def char_counter(string_array: list):
-    char_count = 0
+
+def char_count(string_array: list):
+
+    count = 0
     if len(string_array) == 0:
-        return char_count
+        return count
+    for char in string_array[0]:
+        count += 1
+    return count + char_count(string_array[1:])
 
-    char_counter(string_array - string_array[0])
+string_array = ["afger", "asdgkkg", "batman"]
 
-    for idx_str in string_array:
-        char_count += 1
+print(char_count(string_array))
+
+def even_nums(num_array: list):
+    even_num = []
+    if len(num_array) == 0:
+        return even_num
+    if num_array[0] % 2 == 0:
+        even_num.append(num_array[0])
+        return even_num, even_nums(num_array[1:])
+    else:
+        return even_nums(num_array[1:])
+
+test_nums = [1, 2, 3, 4, 5, 6, 7, 8]
+print(even_nums(test_nums))
