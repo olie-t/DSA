084991459e1ac619d787b1662367a029080aed7e
Olie <olthomas13@gmail.com>
2024-02-23 09:37:17 +0000

    adding ladder case

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,19 +1,17 @@
 
 
 def char_count(string_array: list):
-
-    count = 0
+    #count the number of characters in an array of strings
     if len(string_array) == 0:
-        return count
-    for char in string_array[0]:
-        count += 1
-    return count + char_count(string_array[1:])
+        return 0
+    return len(string_array[0]) + char_count(string_array[1:])
 
 string_array = ["afger", "asdgkkg", "batman"]
 
 print(char_count(string_array))
 
 def even_nums(num_array: list):
+    #return an array of even numbers from an array of numbers
     even_num = []
     if len(num_array) == 0:
         return even_num
@@ -33,12 +31,18 @@ def triangle_numbers(num: int):
 
 print(triangle_numbers(8))
 
-def letter_find(string: str):
-    idx = 0
-    if string[0] == 'x':
-        return idx
-    else:
-        idx += 1
-    return letter_find(string[1:]) + idx
+def letter_find(string: str, letter: str) -> int:
+    #find the index of a given letter within a string
+    if string[0] == letter:
+        return 0
+    return letter_find(string[1:]) + 1
 
-print(letter_find("abctrfht"))
+print(letter_find("abctrfxht"))
+
+def find_routes(x: int, y: int) -> int:
+    #find the amount of shortest routes over a grid described by x * y
+    if x == 1 or y == 1:
+        return 1
+    return find_routes(x - 1, y) + find_routes(x, y - 1)
+
+print(find_routes(5,3))
